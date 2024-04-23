ff98d5432848d36e21d25049d4ceea19612da3ad
Joe Stubbs <joestubbs5@gmail.com>
2024-04-23 12:29:14 -0500

    updates to grader script

--- project3-grader.py
+++ project3-grader.py
@@ -4,16 +4,36 @@ import requests
 import tensorflow as tf 
 
 
-base_url = "http://172.17.0.1:5001"
+base_url = "http://172.17.0.1:5000"
 
 # UPDATE: Config  ----
 
 # george
 # path = "/models/damage/v1"
 
-# ashton
-path = "/models/hurricane-damage/v1"
+# path = "/models/hurricane_damage/v1"
 
+# path = "/models/alt_L5/info"
+# path = "/models/alt_L5/process"
+
+# path = "/models/hurricane/v1"
+# path = "/models/alt_lenet5"
+
+# path = "/model/info"
+# path = "/model/predict"
+
+# path = "/info"
+# path = "/predict"
+
+# path = "/models"
+
+# path = "/model_summary"
+# path = "/classify_image"
+
+# path = "/model/info"
+# path = "/model/predict"
+
+path = "/models/best"
 
 # -----------
 
@@ -26,7 +46,9 @@ url = f"{base_url}{path}"
 def make_get_request():
     rsp = requests.get(url)
     print(f"Status code: {rsp.status_code}")
-    print(f"JSON: {rsp.json()}")
+    # UPDATE:
+    print(rsp.content)
+    # print(f"JSON: {rsp.json()}")
 
 
 # POST -----
@@ -40,19 +62,32 @@ def get_paths():
 
 
 def do_custom_processing(path):
-   from tensorflow.keras.layers.experimental.preprocessing import Rescaling
-   img = tf.keras.utils.load_img(
-       path,
-       color_mode='rgb',
-       target_size=(128,128),
-       interpolation='nearest',
-       keep_aspect_ratio=True
-   )
-   rescale = Rescaling(scale=1.0/255)
-   img_res = rescale(img)
-   tens = tf.convert_to_tensor(img_res)
-   inp = tf.expand_dims(tens, 0, name=None).numpy().tolist()
-   return inp 
+#    from tensorflow.keras.layers.experimental.preprocessing import Rescaling
+#    img = tf.keras.utils.load_img(
+#        path,
+#        color_mode='rgb',
+#        target_size=(128,128),
+#        interpolation='nearest',
+#        keep_aspect_ratio=True
+#    )
+#    rescale = Rescaling(scale=1.0/255)
+#    img_res = rescale(img)
+#    tens = tf.convert_to_tensor(img_res)
+#    inp = tf.expand_dims(tens, 0, name=None).numpy().tolist()
+#    return inp 
+
+    from PIL import Image
+    import numpy as np 
+    # img = Image.open(path)
+    # img = np.asarray(img)
+    # return img.tolist()
+    img = Image.open(path).resize((128, 128))
+    img_array = np.array(img) / 255.0
+    img_list = np.expand_dims(img_array, axis=0).tolist()
+    return img_list
+    # from matplotlib import image
+    # img = image.imread(path)
+    # return img.tolist()
 
 
 def make_post_request(path):
@@ -64,14 +99,17 @@ def make_post_request(path):
 
     # B) alternatively, do some custom processing
     image = do_custom_processing(path)
+    # send JSON data 
+    rsp = requests.post(url, json={"image": image})
+    # rsp = requests.post(url, json={"path": path})
     # -----------
 
-    # UPDATE: Check this ---
-    data = {"image": image}
+    # send multipart
+    # data = {"image": image}
+    # rsp = requests.post(url, files=data)
     # ------
 
-    rsp = requests.post(url, json={"image": image})
-    
+    # print(f"POST status code: {rsp.status_code}")
     try:
         rsp.raise_for_status()
     except Exception as e:
@@ -85,14 +123,64 @@ def make_post_request(path):
 def get_prediction(result, label):
     # UPDATE
     probs = result['result'][0]
-    if probs[0] > probs[1]:
+    if probs[0] < 0.5:
         prediction = "damage"
     else:
         prediction = "no_damage"
+    # if probs[0] > probs[1]:
+    #     prediction = "damage"
+    # else:
+    #     prediction = "no_damage"
     if prediction == label:
         return 1
     else:
         return 0
+    
+    # prediction = result["result"]
+    # prediction = result["prediction"]
+    # if "No Damage" in prediction: 
+    #     if label == "no_damage":
+    #         return 1
+    #     else:
+    #         return 0
+    # elif label == "damage":
+    #     return 1
+    # else: 
+    #     return 0
+
+    # if prediction == "contains no damage" and label == "no_damage":
+    #     return 1
+    # elif prediction == "contains damage" and label == "damage":
+    #     return 1
+    # else: 
+    #     return 0
+
+def do_full_post_test():
+    print("Starting full POST test suite...")
+    total_correct = 0
+    total = 0
+
+    for p in damage_paths:
+        total += 1
+        result = make_post_request(p)
+        if result:
+            prediction = get_prediction(result, "damage")
+            total_correct = total_correct + prediction 
+
+    for p in no_damage_paths:
+        total += 1
+        result = make_post_request(p)
+        if result:
+            prediction = get_prediction(result, "no_damage")
+            total_correct = total_correct + prediction 
+
+    accuracy = float(total_correct)/float(total)
+
+    print("Final results:")
+    print(f"Total correct: {total_correct}")
+    print(f"Total: {total}")
+    print(f"Accuracy: {accuracy}")
+
 
 
 print("\n\n\n**** STARTING GRADING ****\n")
@@ -100,29 +188,11 @@ make_get_request()
 damage_paths, no_damage_paths = get_paths()
 
 # quick test of the POST
-print(make_post_request(damage_paths[0]))
+print(make_post_request(no_damage_paths[1]))
+# for i in range(len(no_damage_paths)):
+#     print(make_post_request(no_damage_paths[i]))
 
-total_correct = 0
-total = 0
+# print(make_post_request(damage_paths[2]))
 
-for p in damage_paths:
-    total += 1
-    result = make_post_request(p)
-    if result:
-        prediction = get_prediction(result, "damage")
-        total_correct = total_correct + prediction 
-
-for p in no_damage_paths:
-    total += 1
-    result = make_post_request(p)
-    if result:
-        prediction = get_prediction(result, "no_damage")
-        total_correct = total_correct + prediction 
-
-accuracy = float(total_correct)/float(total)
-
-print("Final results:")
-print(f"Total correct: {total_correct}")
-print(f"Total: {total}")
-print(f"Accuracy: {accuracy}")
+do_full_post_test()
 
